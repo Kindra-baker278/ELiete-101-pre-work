df0635580dd5386ec6c45dd0237448c149251f00
Kindra-baker278 <kinkin27.bake8@gmail.com>
2025-09-20 05:37:31 +0000

    all elif things doen today related to menu options and if and elif

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 print('welcome to the bakery special ordering chatbot')
 name= input('What is your name?')
-age = input('Hello' + name + ',how old are you')
+age = int(input('Hello' + name + ',how old are you'))
+if age<14:
+ print ('too young to order form our bakery, ask a parent or gaurdian')
+elif age<18:
+ print('Ask adult permission before buying, press next to order')
+elif age>75:
+ print('Please comfirm witha younger adult if your order is correct')
+elif age>18:
+ print('Every adut deserves a tasty treat')
+else:
+ print('you must be an alien')
+
+print('what would you like to order')
+def order_menu():
+  print("n/---main Menu---")
+  print('1.cake slices & cake')
+  print('2.pastries')
+  print('3.special cake orders')
+  print('4.cofee and and appetizers')
+  menu_choice = input('>'). lower()
